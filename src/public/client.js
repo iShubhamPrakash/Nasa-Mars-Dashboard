c719a7d8c0b5368b5d086b68dad8b17665ef8a80
Shubham Prakash <shubham.prakash2308@gmail.com>
2020-02-18 19:59:44 +0530

    Basic feature ready

--- src/public/client.js
+++ src/public/client.js
@@ -1,13 +1,15 @@
 let store = {
-    user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+    currentTab: 'spirit',
+    roverInfo: {},
+    roverImages: [],
 }
 
 // add our markup to the page
-const root = document.getElementById('root')
+const root = document.getElementById('root');
+const tabs= document.querySelectorAll('.tab');
 
 const updateStore = (store, newState) => {
+    console.log("Updating the store-",newState);
     store = Object.assign(store, newState)
     render(root, store)
 }
@@ -19,87 +21,121 @@ const render = async (root, state) => {
 
 // create content
 const App = (state) => {
-    let { rovers, apod } = state
+    let { roverInfo, roverImages } = state
 
     return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
+        <div>
+            <div class="info-container">
+                ${renderInfo(roverInfo)}
+            </div>
+            <section class="image-container">
+                ${renderImages(roverImages)}
             </section>
-        </main>
-        <footer></footer>
+        </div>
     `
 }
 
+const fetchData= async (store,currentTab)=>{
+    await getRoverData(store,currentTab);
+    await getRoverImages(store,currentTab);
+}
 // listening for load event because page should load before any JS is called
-window.addEventListener('load', () => {
-    render(root, store)
+window.addEventListener('load', async () => {
+    console.log("Window loaded-");
+    init(tabs,store);
+    await fetchData(store,store.currentTab);
+    render(root, store);
 })
+
+const init = async (tabs,store)=>{
+    tabs.forEach(tab => {
+        tab.addEventListener('click',async e => {
+            const currentTab=e.target.id;
+            await updateStore(store,{currentTab: currentTab});
+            activeTab(tabs,currentTab);
+            fetchData(store,currentTab);
+        })
+    });
+}
+
+const activeTab = (tabs,currentTab)=>{
+    tabs.forEach(tab=>{
+        if(tab.id ===currentTab){
+            tab.classList.add('active')
+        }else{
+            tab.classList.remove('active')
+        }
+    })
+}
 
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
-
+// Pure function that renders conditional information --
+const renderInfo = (info) => {
     return `
-        <h1>Hello!</h1>
+        <figure>
+            <img src="${info.imageUrl}" alt="${info.name}" class="main-rover-img"/>
+            <figcaption>An image of ${info.name} rover</figcaption>
+        </figure>
+
+        <div class="info">
+            <strong>About</strong>
+            <p>${info.about}</p>
+
+            <strong>Launch Date</strong>
+            <p>${info.launchDate}</p>
+
+            <strong>Landing Date</strong>
+            <p>${info.landingDate}</p>
+
+            <strong>Max Speed</strong>
+            <p>${info.maxSpeed}</p>
+
+            <strong>Cost</strong>
+            <p>${info.cost}</p>
+
+            <strong>Distance covered</strong>
+            <p>${info.distanceCovered}</p>
+        </div>
     `
 }
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
-
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
+// A pure function that renders images requested from the backend
+const renderImages = (images) => {
+    return images.map(image => {
         return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
+            <div class="image-card">
+                <img src="${image.img_src}" />
+
+            </div>
         `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
+    })
 }
 
 // ------------------------------------------------------  API CALLS
 
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
-
-    fetch(`http://localhost:3000/apod`)
+const getRoverData = (store,roverName) => {
+    console.log("Fetching rover data-");
+    fetch(`http://localhost:3000/roverInfo`,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({roverName:roverName})
+    })
         .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+        .then(roverInfo => updateStore(store, { roverInfo: roverInfo }))
+}
 
-    return data
+const getRoverImages = (store,roverName) => {
+    console.log("Fetching rover images-");
+    fetch(`http://localhost:3000/fetchImage`,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({roverName:roverName})
+    })
+        .then(res => res.json())
+        .then(roverImages => updateStore(store, { roverImages: roverImages }))
 }
--- src/server/index.js
+++ src/server/index.js
@@ -7,12 +7,70 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+const roverData={
+    curiosity: {
+        name:"Curiosity",
+        about: "Curiosity is a car-sized rover designed to explore the crater Gale on Mars as part of NASA's Mars Science Laboratory mission. Curiosity was launched from Cape Canaveral on November 26, 2011, at 15:02 UTC and landed on Aeolis Palus inside Gale on Mars on August 6, 2012, 05:17 UTC.",
+        launchDate:"26 November 2011, 8:32 pm IST",
+        maxSpeed: "0.14 km/h",
+        landingDate:"August 6, 2012, 05:17:57 UTC",
+        cost: "250 crores USD (2012)",
+        distanceCovered: "21.61 km (13.43 mi); as of 1 January 2020",
+        imageUrl: "./assets/curiosity.webp"
+    },
+    opportunity: {
+        name:"Opportunity",
+        about: "Opportunity, also known as MER-B or MER-1, and nicknamed 'Oppy', is a robotic rover that was active on Mars from 2004 until the middle of 2018",
+        launchDate:"8 July 2003, 8:48 am IST",
+        maxSpeed: "0.18 km/h",
+        landingDate:"January 25, 2004, 05:05 UTC SCET; MSD 46236 14:35 AMT",
+        cost: "40 crores USD",
+        distanceCovered: " 45.16 km (28.06 mi)",
+        imageUrl: "./assets/opportunity.png"
+    },
+    spirit: {
+        name:"Spirit",
+        about: "Spirit, also known as MER-A or MER-2, is a robotic rover on Mars, active from 2004 to 2010. It was one of two rovers of NASA's Mars Exploration Rover Mission.",
+        launchDate:"10 June 2003, 11:28 pm IST",
+        maxSpeed: "0.18 km/h",
+        landingDate:"January 4, 2004, 04:35 UTC SCET; MSD 46216 03:35 AMT",
+        cost: "40 crores USD",
+        distanceCovered: "-",
+        imageUrl: "./assets/spirit.jpg"
+    },
+
+};
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-// your API calls
+// Send information about the rover
+app.post('/roverInfo', async (req, res) => {
+    try {
+        console.log("Call for ", req.body.roverName);
+        res.send(roverData[req.body.roverName]);
+
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+// Fetching images from NASA Apis
+app.post('/fetchImage', async (req, res) => {
+    try {
+        console.log("Call for ", req.body.roverName);
+        const URL=`https://api.nasa.gov/mars-photos/api/v1/rovers/${req.body.roverName}/photos?sol=1000&api_key=${process.env.API_KEY}`
+        let data = await fetch(URL)
+            .then(res => res.json())
+            //send data
+            res.send(data.photos)
+
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
 
 // example API call
 app.get('/apod', async (req, res) => {
@@ -25,4 +83,4 @@ app.get('/apod', async (req, res) => {
     }
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
