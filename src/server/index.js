643b3f995b889af8c310e8fc4e924fffd56fdea4
Shubham Prakash <shubham.prakash2308@gmail.com>
2020-03-13 08:30:11 +0530

    Used immutable js

--- src/server/index.js
+++ src/server/index.js
@@ -7,8 +7,11 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-const roverData={
-    curiosity: {
+// Use of immutable js Map
+const { Map } = require('immutable')
+
+const roverData=Map({
+    curiosity: Map({
         name:"Curiosity",
         about: "Curiosity is a car-sized rover designed to explore the crater Gale on Mars as part of NASA's Mars Science Laboratory mission. Curiosity was launched from Cape Canaveral on November 26, 2011, at 15:02 UTC and landed on Aeolis Palus inside Gale on Mars on August 6, 2012, 05:17 UTC.",
         launchDate:"26 November 2011, 8:32 pm IST",
@@ -17,8 +20,8 @@ const roverData={
         cost: "250 crores USD (2012)",
         distanceCovered: "21.61 km (13.43 mi); as of 1 January 2020",
         imageUrl: "./assets/curiosity.webp"
-    },
-    opportunity: {
+    }),
+    opportunity: Map({
         name:"Opportunity",
         about: "Opportunity, also known as MER-B or MER-1, and nicknamed 'Oppy', is a robotic rover that was active on Mars from 2004 until the middle of 2018",
         launchDate:"8 July 2003, 8:48 am IST",
@@ -27,8 +30,8 @@ const roverData={
         cost: "40 crores USD",
         distanceCovered: " 45.16 km (28.06 mi)",
         imageUrl: "./assets/opportunity.png"
-    },
-    spirit: {
+    }),
+    spirit: Map({
         name:"Spirit",
         about: "Spirit, also known as MER-A or MER-2, is a robotic rover on Mars, active from 2004 to 2010. It was one of two rovers of NASA's Mars Exploration Rover Mission.",
         launchDate:"10 June 2003, 11:28 pm IST",
@@ -37,9 +40,9 @@ const roverData={
         cost: "40 crores USD",
         distanceCovered: "-",
         imageUrl: "./assets/spirit.jpg"
-    },
+    }),
 
-};
+});
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -49,7 +52,7 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 // Send information about the rover
 app.post('/roverInfo', async (req, res) => {
     try {
-        res.send(roverData[req.body.roverName]);
+        res.send(roverData.get(req.body.roverName));
 
     } catch (err) {
         console.log('error:', err);
